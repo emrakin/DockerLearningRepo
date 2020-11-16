b667a5ad6543a36533a2cd28a889de1e7724aa0a
Emrah AKIN <eakin@uzmansistem.com.tr>
2020-11-16 18:42:22 +0300

    ilkcontainer

---
+++ CreateDockerImage/CreateDockerImage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CreateDockerImage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba İlk Container Başarıyla Oluşturuldu");
+            Console.ReadLine();
+        }
+    }
+}
